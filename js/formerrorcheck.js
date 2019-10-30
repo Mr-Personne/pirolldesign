e8c16ce980ce5f43d95aec8a5e242bd37849c0f4
Spriinty <quentinpetrequin@gmail.com>
2019-10-30 09:17:23 +0000

    ajout dossier JS + fichier javascript formulaire

---
+++ js/formerrorcheck.js
@@ -0,0 +1,15 @@
+const myForm = document.querySelector('form');
+myForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    elementsForm = myForm.elements;
+    for( let item of elementsForm ){
+       if( !item.validity.valid ){
+          item.classList.remove('is-valid');
+           item.classList.add('is-invalid');
+       }
+       else{
+         item.classList.remove('is-invalid');
+         item.classList.add('is-valid');
+        }
+    }
+});
